fed214697e74ae439d967632f805a2a5e747b85b
Naresh Krish <narioinc89@gmail.com>
2022-09-05 14:34:56 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.headers)
+  if(req.headers.authorization && req.headers.authorization.includes('narioinc89')){
+    res.status(200)
+    res.json({})
+  }
+  else{
+    res.status(403)
+    res.json({"message": "failed to authenticate"})
+  }
+});
+
+module.exports = router;

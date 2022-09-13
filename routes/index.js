aed1cac5f1fd52be3a439cd48fd04406e15e9b72
naresh.krish <naresh@email.com>
2022-09-13 10:27:58 +0530

    removed unused routes

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/live', function(req, res, next) {
-  res.status(200)
-  res.json({})
-});
-
-router.get('/ready', function(req, res, next) {
-  res.status(200)
-  res.json({})
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.headers)
   if(req.headers.authorization && req.headers.authorization.includes('narioinc89')){
     res.status(200)
-    res.json({})
+    res.json({"status": "ok"})
   }
   else{
     res.status(403)
